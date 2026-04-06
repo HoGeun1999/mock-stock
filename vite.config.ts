d6302e6fbc6cd92fcf29f410dcc1c9e144a5c61d
needer-ho <needer_ho@gubgoo.com>
2026-04-06 21:42:19 +0900

    feat: api and cors setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/kis-api": {
+        target: "https://openapi.koreainvestment.com:29443", // 모의투자 주소
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/kis-api/, ""),
+        secure: false,
+      },
+    },
+  },
+});
